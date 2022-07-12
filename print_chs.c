7678cdd16ba426287c611f83f10857aaad4a35cf
Elias Shamil <eliasshamil@gmail.com>
2022-07-12 10:25:55 -0700

    printf

--- print_chs.c
+++ print_chs.c
@@ -38,29 +38,6 @@ int print_s(va_list s)
 	}
 		return (i);
 }
-/**
- * hex_print - prints a char's ascii value in uppercase hex
- * @c: char to print
- *
- * Return: number of chars printed (always 2)
- */
-static int hex_print(char c)
-{
-	int count;
-	char diff = 'A' - ':';
-	char d[2];
-
-	d[0] = c / 16;
-	d[1] = c % 16;
-	for (count = 0; count < 2; count++)
-	{
-		if (d[count] >= 10)
-			_putchar('0' + diff + d[count]);
-		else
-			_putchar('0' + d[count]);
-	}
-	return (count);
-}
 
 /**
  * print_S - prints a string and nonprintable character ascii values
