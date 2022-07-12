cf8390c3dcdf9b06e0c167338f173364de78a476
Elias Shamil <eliasshamil@gmail.com>
2022-07-12 06:13:23 -0700

    printf

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
 /**
  * struct pt - tydef struct for conversion specifiers
  * @pt: struct print
