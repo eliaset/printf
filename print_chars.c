405e1da168f88a20536d6b15092c7db834503ae1
Elias Shamil <eliasshamil@gmail.com>
2022-07-12 09:04:42 -0700

    printf

--- print_chars.c
+++ print_chars.c
@@ -39,22 +39,6 @@ int print_s(va_list s)
 		return (i);
 }
 /**
- * print_pe - prints percent sign
- * @args: percent sign
- *
- * Return: number of character printed
- * in this case, return value will always be 1
- */
-
-int print_pe(va_list __attribute__((unused)) pe)
-{
-	char perc = '%';
-
-	_putchar(perc);
-
-	return (1);
-}
-/**
  * hex_print - prints a char's ascii value in uppercase hex
  * @c: char to print
  *
